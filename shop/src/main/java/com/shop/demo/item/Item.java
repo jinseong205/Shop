58fe4c026f1aa917b04805310577a37bd6678102
jinseong <jinseong205@gmail.com>
2023-04-28 01:07:01 +0900

    ^^7

--- shop/src/main/java/com/shop/demo/item/Item.java
+++ shop/src/main/java/com/shop/demo/item/Item.java
@@ -2,7 +2,14 @@ package com.shop.demo.item;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import com.shop.demo.constant.ItemSellStatus;
 
@@ -13,13 +20,61 @@ import lombok.ToString;
 @ToString
 @Entity
 public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false, length = 100)
 	private String itemName;
+	
+	@Column(nullable = false)
 	private int pirce;
+	
+	@Column(nullable = false)
 	private int stockNum;
+
+	@Lob
 	private String Detail;
+	
 	private ItemSellStatus itemSellStatus;
+	
+	@Enumerated(EnumType.STRING)
 	private LocalDateTime crtDt;
+	
 	private LocalDateTime updtDt;
 	
+	@Column(length = 200)
+	private String attr1;
+
+	@Column(length = 200)
+	private String attr2;
+
+	@Column(length = 200)
+	private String attr3;
+
+	@Column(length = 200)
+	private String attr4;
+
+	@Column(length = 200)
+	private String attr5;
+
+	@Column(length = 200)
+	private String attr6;
+
+	@Column(length = 200)
+	private String attr7;
+
+	@Column(length = 200)
+	private String attr8;
+
+	@Column(length = 200)
+	private String attr9;
+
+	@Column(length = 200)
+	private String attr10;
+
+	
+	
 }
+
