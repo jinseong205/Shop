75db753cf9330a824d85a1e3ba814c6c9ffd5796
jinseong <jinseong205@gmail.com>
2023-06-05 21:03:05 +0900

    ^^7

--- shop/src/main/java/com/shop/demo/order/Order.java
+++ shop/src/main/java/com/shop/demo/order/Order.java
@@ -1,6 +1,8 @@
 package com.shop.demo.order;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,7 +10,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.shop.demo.user.User;
@@ -25,12 +29,16 @@ public class Order {
 	private Long id;
 	
 	@ManyToOne
+	@JoinColumn(name="member_id")
 	private User user;
 	
 	private LocalDateTime ordDt;
 	
 	@Enumerated(EnumType.STRING)
 	private OrderStatus orderStatus;
+	
+	@OneToMany(mappedBy="order")	//연관관계의 주인이 아님. 
+	private List<OrderProduct> orderProducts = new ArrayList<>();
 	
 	private LocalDateTime regDt;
 	
--- shop/src/main/java/com/shop/demo/order/OrderProduct.java
+++ shop/src/main/java/com/shop/demo/order/OrderProduct.java
@@ -1,5 +1,38 @@
 package com.shop.demo.order;
 
-public class OrderProduct {
+import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.shop.demo.product.Product;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class OrderProduct {
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 	
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="id")
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name= "id")
+	private Order order;
+
+	private int orderPrice;
+
+	private int count;
+
+	private LocalDateTime regDt;
+	
+	private LocalDateTime updtDt;
 }
