8853e73db97777cdcea4ff2a57bd7bfa35b1ea30
jinseong <jinseong205@gmail.com>
2023-07-13 03:05:12 +0900

    ^^7

--- shop/src/main/java/com/shop/demo/controller/TestController.java
+++ shop/src/main/java/com/shop/demo/controller/TestController.java
@@ -51,13 +51,15 @@ public class TestController {
 		return new ResponseEntity<>("OOO", HttpStatus.OK);
 	}
 
+	@PostMapping("api/test/file2")
+	public ResponseEntity<?> itemNew2(@RequestPart(name = "itemImgFile") MultipartFile itemImgFile) throws Exception {
+		log.debug("-----" + itemImgFile.getSize());
+		return new ResponseEntity<>("OOO", HttpStatus.OK);
+	}
 
 	@PostMapping(value = "/api/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<?> handleFileUpload(@RequestParam("dto") String dto, @RequestParam("file") MultipartFile file) {
 	 
-		// DTO 및 파일을 처리하는 로직을 여기에 작성하세요.
-		// 파일 저장, 데이터베이스에 저장 등의 작업을 수행할 수 있습니다.
-
 		// 예시로서, 성공적인 응답을 반환합니다.
 		return new ResponseEntity<>("OOO", HttpStatus.OK);
 	}
