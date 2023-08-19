2b22c5396cee13beb53ffa2a8dcf39badf55f616
jinseong <jinseong205@gmail.com>
2023-08-19 22:14:53 +0900

    ^^7

--- shop/src/main/java/com/shop/demo/error/ApiExceptionHandler.java
+++ shop/src/main/java/com/shop/demo/error/ApiExceptionHandler.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ApiExceptionHandler {
 
-	//@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler
 	public ResponseEntity<?> commonExceptionHandler(Exception e) {
 		
